94686d6d4873e656a5371b2f31333a3c4af4eb6f
jcollins0 <8459239+JCollins0@users.noreply.github.com>
2023-11-10 09:43:24 -0600

    Fix inject script, minor changes

--- projects/header/src/inject.js
+++ projects/header/src/inject.js
@@ -1,11 +1,19 @@
-const header = document.createElement("vdp-header");
-const yourspot = document.getElementById("vdp-navbar");
-if (yourspot) {
-  yourspot.appendChild(header);
-}
+(function injectMe() {
+  const existingHeader = document.getElementsByTagName("vdp-header");
+  const existingFooter = document.getElementsByTagName("vdp-footer");
 
-const footer = document.createElement("vdp-footer");
-const yourFooter = document.getElementById("footer");
-if (yourFooter) {
-  yourFooter.appendChild(footer);
-}
+  if (!existingHeader.length) {
+    const header = document.createElement("vdp-header");
+    const yourspot = document.getElementById("vdp-navbar");
+    if (yourspot) {
+      yourspot.appendChild(header);
+    }
+  }
+  if (!existingFooter.length) {
+    const footer = document.createElement("vdp-footer");
+    const yourFooter = document.getElementById("footer");
+    if (yourFooter) {
+      yourFooter.appendChild(footer);
+    }
+  }
+})();
